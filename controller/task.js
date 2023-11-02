1a8de1cac56876822138d621ca370eaee819e385
Felicien1992 <felicienmukesharugamba@gmail.com>
2023-11-02 22:12:01 +0200

    add all methode

--- controller/task.js
+++ controller/task.js
@@ -1,31 +1,38 @@
 const Task = require('../models/Task')
 
-const getAllTask = (req, res) => {
+const getAllTask = async (req, res) => {
     // res.send('Get all task')
+    const alltask = await Task.get(req, res)
+    res.status(201).json({ alltask })
     res.json(req.body)
 };
 
 const createTask = async (req, res) => {
 
     // const task = await Task.create({name:'first task'})
-     const task = await Task.create(req.body)
-     res.status(201).json({task})
-     res.json(req.body)
-    // res.json(req.body);
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+    res.json(req.body)
     // res.send('create One Task');
 };
 
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
+    const getTask = await Task.get(req, res)
+    res.status(200).json({ getTask })
     res.json({ id: req.params.id });
     // res.send('Get One Task');
 };
 
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
+    const update = await Task.post(req, res)
+    res.status(201).json({ update })
     res.json(req.body);
     // res.send('update Task');
 };
 
-const deleteTask = (req, res) => {
+const deleteTask = async (req, res) => {
+    const delet = await Task.delete(req, res)
+    res.status(200).json({ delet })
     // res.send('deleteTask');
     res.json({ id: req.params.id });
 };
--- models/Task.js
+++ models/Task.js
@@ -6,7 +6,9 @@ const TaskSchema = new mongoose.Schema({
         trim:true,
         maxlength:[20,'name can not more than 20 character']
     },
-    completed:Boolean
+    completed:Boolean,
+    phoneNumber: Number,
+    address: String
 })
 
 module.exports = mongoose.model('Task',TaskSchema)
